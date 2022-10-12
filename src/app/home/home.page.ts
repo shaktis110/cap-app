543eae4a277d329fa6fae2317f8c38c3d43174d4
Unknown user <38592947+shaktis110@users.noreply.github.com>
2022-10-12 17:34:57 +0530

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-
+import { FirebaseX } from '@awesome-cordova-plugins/firebase-x/ngx';
+import { AlertController,Platform } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
+token;
+  constructor(private firebasex: FirebaseX,private alert: AlertController,private platform: Platform) {
+this.platform.ready().then(() =>{
+  this.firebasex.getToken()
+  .then(token => {console.log(`The token is ${token}`);
+  this.token = token;
+}).catch(error => console.error('Error getting token', error));
+});
 
 }
