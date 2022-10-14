2d79667bf5bba179c123cae9fd1d91b9a9c2b375
Unknown user <38592947+shaktis110@users.noreply.github.com>
2022-10-14 21:01:12 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { FirebaseX } from '@awesome-cordova-plugins/firebase-x/ngx';
+import { StatusBar } from '@awesome-cordova-plugins/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -11,7 +12,7 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },FirebaseX],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },FirebaseX,StatusBar],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
